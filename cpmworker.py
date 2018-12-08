22c61564971c1b6c61c108bc45b6397bb789472d
Mike Topskiy <36136553+broadnetty@users.noreply.github.com>
2018-12-08 18:58:04 +0300

    added test file

--- cpmworker.py
+++ cpmworker.py
@@ -2,8 +2,9 @@ import logging
 import requests
 import urllib3
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#@2323
 
 #Enable logging
 logger = logging.getLogger(__name__)
@@ -161,11 +162,5 @@ class CPMworker:
         return response.json()
 
 
-if __name__ == '__main__':
 
-    host = "ec2-18-235-133-229.compute-1.amazonaws.com"
-    api_key = "b9490fab3f625213809e87fab64a818b4b60041ea5c59c4405117fc4ede350214ede059b736c2010754906421bd95c8c742178f696ac395d769f5304cffa30ee"
-
-    wk = CPMworker(host, api_key)
-    print(wk.get_accounts())
 
