011e810db338971d57ed13896da747ba4423b3b9
Mike Topskiy <36136553+broadnetty@users.noreply.github.com>
2018-12-08 18:54:49 +0300

    added test file

---
+++ main_tests.py
@@ -0,0 +1,9 @@
+from cpmworker import *
+
+if __name__ == '__main__':
+
+    host = "ec2-18-235-133-229.compute-1.amazonaws.com"
+    api_key = "b9490fab3f625213809e87fab64a818b4b60041ea5c59c4405117fc4ede350214ede059b736c2010754906421bd95c8c742178f696ac395d769f5304cffa30ee"
+
+    wk = CPMworker(host, api_key)
+    print(wk.get_accounts())
