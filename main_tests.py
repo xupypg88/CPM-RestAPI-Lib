738044e70f58478fc8aeb0c4fb2bc9a8f61fa8fa
Mike Topskiy <36136553+broadnetty@users.noreply.github.com>
2018-12-10 23:10:00 +0300

    added test file

--- main_tests.py
+++ main_tests.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     api_key = "b9490fab3f625213809e87fab64a818b4b60041ea5c59c4405117fc4ede350214ede059b736c2010754906421bd95c8c742178f696ac395d769f5304cffa30ee"
 
     wk = CPMworker(host, api_key)
-
+#
     print(wk.get_accounts())
