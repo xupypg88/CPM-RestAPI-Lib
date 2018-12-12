ec76f557696e701d025099221f4d86e9f7b7f0d4
xupypg88 <xupypg88@gmail.com>
2018-12-12 21:48:12 +0300

    tests version 1.0

--- main_tests.py
+++ main_tests.py
@@ -1,7 +1,12 @@
 from cpmworker import *
+import os
+
+host=os.environ.get('CPMAPIHOST')
+api_key=os.environ.get('CPMAPIKEY')
+
+# todo write tests based on unittest module
 
 if __name__ == '__main__':
-
 
     wk = CPMworker(host, api_key)
 
