18834427bd807e119353be124bfb5c81c745e55b
xupypg88 <xupypg88@gmail.com>
2018-12-13 00:39:54 +0300

    added correct version of python 2.7

--- main_tests.py
+++ main_tests.py
@@ -16,10 +16,11 @@ class APIFunctionsUsersCase(unittest.TestCase):
     def test_getaccountslist(self):
         predefined = [{'id': 2, 'user': 1, 'name': 'main_backup3', 'authentication': 'R', 'is_dr_account': False, 'scan_tagged_resources': False, 'scan_regions': [2], 'capture_vpcs': True}, {'id': 3, 'user': 1, 'name': 'dr', 'authentication': 'R', 'is_dr_account': True, 'allow_deleting_snapshots': True}, {'id': 4, 'user': 1, 'name': 'esche', 'authentication': 'R', 'is_dr_account': False, 'scan_tagged_resources': False, 'scan_regions': [2], 'capture_vpcs': True}, {'id': 5, 'user': 2, 'name': 'grizzly2_tied', 'authentication': 'A', 'is_dr_account': False, 'account_number': '124556737267', 'role_name': 'Mikhail.Topskiy', 'external_id': None, 'assume_from_account': 2, 'scan_tagged_resources': False, 'scan_regions': [2], 'capture_vpcs': True}]
         wk = CPMworker(HOST, API_KEY)
-        self.assertEqual(wk.get_accounts(), predefined)
+        self.assertEqual(wk.get_accounts(), predefined,"Passed!")
         return
 
     def test_getuserslist(self):
+        self.assertEqual(1,2)
         return
 
     def test_createuser(self):
